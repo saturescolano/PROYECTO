d7931b945a3ade3e9db8e5a37a39329166a70277
SATURNINO ESCOLANO LÓPEZ <saturescolano@gmail.com>
2026-05-15 16:36:20 +0000

    CREACIÓN DE: - generate database - generate schema

---
+++ macros/generate_database_name.sql
@@ -0,0 +1,14 @@
+-- macros/generate_database_name.sql
+-- Macro OPCIONAL pero recomendada. Controla a qué base de datos
+-- se materializa cada modelo. Sin ella +database funciona igual,
+-- pero tenerla hace el routing explícito y fácil de debuggear.
+ 
+{% macro generate_database_name(custom_database_name, node) -%}
+ 
+    {%- if custom_database_name is not none -%}
+        {{ custom_database_name | trim }}
+    {%- else -%}
+        {{ target.database | trim }}
+    {%- endif -%}
+ 
+{%- endmacro %}

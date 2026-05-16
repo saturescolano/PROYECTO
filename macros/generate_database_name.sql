d3c02d9d4851ce9052d0ca4cdcd046fdeeba3125
SATURNINO ESCOLANO LÓPEZ <saturescolano@gmail.com>
2026-05-16 19:50:35 +0000

    he modificado los generate_database_name y schema_name

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -4,12 +4,14 @@
 -- pero tenerla hace el routing explícito y fácil de debuggear.
 
  
+-- macros/generate_database_name.sql
+
 {% macro generate_database_name(custom_database_name, node) -%}
- 
+
     {%- if custom_database_name is not none -%}
-        {{ custom_database_name | trim }}
+        {{ custom_database_name | trim | upper }}
     {%- else -%}
-        {{ target.database | trim }}
+        {{ target.database | trim | upper }}
     {%- endif -%}
- 
+
 {%- endmacro %}
--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,15 +1,16 @@
 -- macros/generate_schema_name.sql
 -- Macro OBLIGATORIA. Sin ella dbt concatena tu usuario al schema:
 -- DEV_SILVER_DB.dbt_usuario_staging en vez de DEV_SILVER_DB.staging
+-- macros/generate_schema_name.sql
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
 
     {%- if custom_schema_name is not none -%}
-        {{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim | upper }}
     {%- else -%}
-        {{ default_schema }}
+        {{ default_schema | upper }}
     {%- endif -%}
 
 {%- endmacro %}

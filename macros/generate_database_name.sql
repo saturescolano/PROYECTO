8b455b803c10b143bffa0d9a2551ab71eea62c6a
SATURNINO ESCOLANO LÓPEZ <saturescolano@gmail.com>
2026-05-17 08:39:11 +0000

    MODIFICACIÓN DE dbt_project.yml (no estaba apuntando bien a Snowflake)

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -3,15 +3,14 @@
 -- se materializa cada modelo. Sin ella +database funciona igual,
 -- pero tenerla hace el routing explícito y fácil de debuggear.
 
- 
 -- macros/generate_database_name.sql
 
 {% macro generate_database_name(custom_database_name, node) -%}
 
     {%- if custom_database_name is not none -%}
-        {{ custom_database_name | trim | upper }}
+        {{ custom_database_name | trim }}
     {%- else -%}
-        {{ target.database | trim | upper }}
+        {{ target.database | trim }}
     {%- endif -%}
 
 {%- endmacro %}
--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,16 +1,13 @@
 -- macros/generate_schema_name.sql
--- Macro OBLIGATORIA. Sin ella dbt concatena tu usuario al schema:
--- DEV_SILVER_DB.dbt_usuario_staging en vez de DEV_SILVER_DB.staging
--- macros/generate_schema_name.sql
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
 
     {%- if custom_schema_name is not none -%}
-        {{ custom_schema_name | trim | upper }}
+        {{ custom_schema_name | trim }}
     {%- else -%}
-        {{ default_schema | upper }}
+        {{ default_schema }}
     {%- endif -%}
 
 {%- endmacro %}

e06dc7023aeddc68560152f05ee9a1e9f03e44a0
SATURNINO ESCOLANO LÓPEZ <saturescolano@gmail.com>
2026-05-18 17:22:42 +0000

    se comitea antes del dbt build para comprobar todo

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -8,9 +8,9 @@
 {% macro generate_database_name(custom_database_name, node) -%}
 
     {%- if custom_database_name is not none -%}
-        {{ custom_database_name | trim }}
+        {{ custom_database_name | trim | upper }}
     {%- else -%}
-        {{ target.database | trim }}
+        {{ target.database | trim | upper }}
     {%- endif -%}
 
 {%- endmacro %}
--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -5,9 +5,9 @@
     {%- set default_schema = target.schema -%}
 
     {%- if custom_schema_name is not none -%}
-        {{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim | upper }}
     {%- else -%}
-        {{ default_schema }}
+        {{ default_schema | upper }}
     {%- endif -%}
 
 {%- endmacro %}
